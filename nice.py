d4e2ff92e9fa9b42e13fecfc77d8fac73434fefa
Dheeraj Jindal <dheerajjindal300@gmail.com>
2021-05-08 20:37:46 +0530

    1

--- nice.py
+++ nice.py
@@ -1,5 +1,9 @@
 hungry = input("Are you Hungry?")
 if hungry == "yes":
     print("Eat something delicius")
+    print("1")
+    print("2")
+    print("3")
+    print("4")
 else:
     print("DO si=ome exercise")
